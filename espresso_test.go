6da53f6acd72d0c4f8c2d4879c1977551dbeb945
mparaiso <aikah@free.fr>
2015-04-26 01:35:17 +0200

    expresso.go updated

--- espresso_test.go
+++ espresso_test.go
@@ -1,31 +1,135 @@
 package expresso_test
 
 import (
+	"bytes"
 	"fmt"
-	"github.com/interactiv/expresso"
-	"github.com/interactiv/expect"
 	"net/http"
-    "net/http/httptest"
+	"net/http/httptest"
+	"net/url"
 	"testing"
+
+	"github.com/interactiv/expect"
+	"github.com/interactiv/expresso"
 )
 
-func TestIntroUsage(t *testing.T) {
-    var (
-        req *http.Request
-        err error
-        
-    )
-	app:= expresso.App()
-    app.Get("/hello/{name}",func(name string,rw http.ResponseWriter){
-        fmt.Fprintf(rw,"Hello %s",name)
-    })
-    w:=httptest.NewRecorder()
-    if req,err=http.NewRequest("GET","http://foobar.com/hello/foo",nil);err!=nil{
-        t.Fatal(err)
-    }
-    app.ServeHTTP(w,req)
-    expect.Expect(w.Code,t).ToBe(200)
-    expect.Expect(w.Body.String(),t).ToBe("Hello foo")
+const formContentType = "application/x-www-form-urlencoded"
+
+func TestHelloWord(t *testing.T) {
+
+	var (
+		req *http.Request
+		err error
+	)
+	app := expresso.App()
+	app.Get("/hello/:name", func(ctx *expresso.Context, rw http.ResponseWriter) {
+		fmt.Fprintf(rw, "Hello %s", ctx.Params["name"])
+	})
+	w := httptest.NewRecorder()
+	if req, err = http.NewRequest("GET", "http://foobar.com/hello/foo", nil); err != nil {
+		t.Fatal(err)
+	}
+	app.ServeHTTP(w, req)
+	expect.Expect(w.Code, t).ToBe(200)
+	expect.Expect(w.Body.String(), t).ToBe("Hello foo")
 }
 
+func TestPost(t *testing.T) {
 
+	app := expresso.App()
+	app.Get("/feedback", func() {
+		t.Fatalf("GET /feedback shouldn't be called on POST /feedback request")
+	})
+	app.Post("/feedback", func(rw http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		expect.Expect(err, t).ToBeNil()
+		message := r.Form.Get("message")
+		expect.Expect(message, t).ToBe("message")
+	})
+
+	w := httptest.NewRecorder()
+	values := url.Values{}
+	values.Add("message", "message")
+	body := new(bytes.Buffer)
+	body.WriteString(values.Encode())
+	req, err := http.NewRequest("POST", "http://foo.com/feedback", body)
+	req.Header.Set("Content-Type", formContentType)
+	expect.Expect(err, t).ToBeNil()
+	app.ServeHTTP(w, req)
+	expect.Expect(w.Code, t).ToBe(200)
+}
+
+func TestPut(t *testing.T) {
+
+	app := expresso.App()
+	id := "10"
+	e := expect.New(t)
+	app.Put("/blog/:id", func(ctx *expresso.Context) {
+		e.Expect(ctx.Params["id"]).ToEqual(id)
+	})
+	req, err := http.NewRequest("PUT", fmt.Sprintf("http://foobar.com/blog/%s", id), nil)
+	e.Expect(err).ToBeNil()
+	res := httptest.NewRecorder()
+	app.ServeHTTP(res, req)
+}
+
+func TestDelete(t *testing.T) {
+
+	app := expresso.App()
+	category := "food"
+	id := "200"
+	e := expect.New(t)
+	app.Delete("/category/:category/product/:id", func(ctx *expresso.Context) {
+		e.Expect(ctx.Params["category"]).ToEqual(category)
+		e.Expect(ctx.Params["id"]).ToEqual(id)
+	})
+	res := httptest.NewRecorder()
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("http://example.com/category/%s/product/%s?foo=bar", category, id), nil)
+	e.Expect(err).ToBeNil()
+	app.ServeHTTP(res, req)
+	e.Expect(res.Code).ToEqual(200)
+}
+func TestMatch(t *testing.T) {
+	e := expect.New(t)
+	app := expresso.App()
+	app.Match("/foo", func(rw http.ResponseWriter) {
+		rw.WriteHeader(http.StatusOK)
+	}).SetMethods([]string{"GET", "POST"})
+	app.Match("/bar", func(rw http.ResponseWriter) {
+		rw.WriteHeader(http.StatusOK)
+	}).SetMethods([]string{"*"})
+	server := httptest.NewServer(app)
+	defer server.Close()
+	res, err := http.Get(server.URL + "/foo")
+	e.Expect(err).ToBeNil()
+	e.Expect(res.StatusCode).ToEqual(200)
+	res, err = http.Post(server.URL+"/foo", formContentType, nil)
+	defer res.Body.Close()
+	e.Expect(err).ToBeNil()
+	e.Expect(res.StatusCode).ToBe(200)
+	req, err := http.NewRequest("PUT", server.URL+"/foo", nil)
+	e.Expect(err).ToBeNil()
+	res, err = http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	e.Expect(err).ToBeNil()
+	e.Expect(res.StatusCode).ToBe(http.StatusNotFound)
+	req, err = http.NewRequest("OPTIONS", server.URL+"/bar", nil)
+	e.Expect(err).ToBeNil()
+	res, err = http.DefaultClient.Do(req)
+	e.Expect(err).ToBeNil()
+	defer res.Body.Close()
+	e.Expect(res.StatusCode).ToEqual(http.StatusOK)
+}
+
+func TestIsCallable(t *testing.T) {
+	var f = func() {}
+	e := expect.New(t)
+	e.Expect(expresso.IsCallable(f)).ToBeTrue()
+	foo := new(Foo)
+	e.Expect(expresso.IsCallable(foo.Call)).ToBeTrue()
+}
+
+type Foo struct{}
+
+func (f Foo) Call() {
+
+}
--- expresso.go
+++ expresso.go
@@ -3,32 +3,224 @@
 
 package expresso
 
-
 import (
+	"fmt"
 	"net/http"
+	"reflect"
+	"regexp"
+	"strings"
 )
 
-type handlerFunc interface{}
+var (
+	// Pattern represents a route param regexp pattern
+	Pattern = "(?:\\:)(\\w+)"
+	// DefaultPatternMatcher represents the default pattern that a route param matches
+	DefaultParamPattern = "(\\w+)"
+)
 
-type Route{
-    Method string
-    Pattern string
-    HandlerFunc interface{}
-    Params []string
+/**********************************/
+/*             ROUTE              */
+/**********************************/
+
+//Route represents a route in the router
+type Route struct {
+	methods     []string
+	pattern     *regexp.Regexp
+	handlerFunc interface{}
+	params      []string
+	frozen      bool
+	converters  map[string]interface{}
 }
 
-func App()*Expresso{
-    return &Expresso{}
+// Params return route variable names.
+// For instance if a route has the following pattern:
+//    /catalog/:category/:productId
+// it will return []string{"category","productId"}
+func (r *Route) Params() []string { return r.params }
+func (r *Route) HandlerFunc() interface{} {
+	return r.handlerFunc
+}
+func (r *Route) SetHandlerFunc(handlerFunc interface{}) {
+	handlerValue := reflect.ValueOf(handlerFunc)
+	if handlerValue.Kind() != reflect.Func {
+		panic("handlerFunc must a function")
+	}
+	r.handlerFunc = handlerFunc
+}
+func (r Route) MethodMatch(method string) bool {
+	match := false
+	for _, m := range r.Methods() {
+		if strings.TrimSpace(strings.ToUpper(method)) == m || m == "*" {
+			match = true
+			break
+		}
+	}
+	return match
+}
+func (r *Route) Freeze() {
+	if r.IsFrozen() == false {
+		r.frozen = true
+	}
 }
 
-type Expresso struct{
-    routes []Route
+// IsFrozen return the frozen state of a route.
+// A Frozen route cannot be modified.
+func (r Route) IsFrozen() bool {
+	return r.frozen
 }
 
-func (e *Expresso) ServeHTTP(responseWriter http.ResponseWriter,request *http.Request) {
-	responseWriter.WriteHeader(http.StatusOK)
+// Methods gets methods handled by the route
+func (r *Route) Methods() []string {
+	return r.methods
+
 }
 
-func (e *Expresso) Get(route string,handlerFunc handlerFunc){
-    route
+// SetMethods sets the methods handled by the route.
+//
+// Example:
+//
+//    route.SetMethods([]string{"GET","POST"})
+// []string{"*"} means the route handles all methods.
+func (r *Route) SetMethods(methods []string) {
+	if r.IsFrozen() == true {
+		return
+	}
+	r.methods = methods
+}
+
+func (r *Route) Convert(param string, converterFunc interface{}) *Route {
+	if r.IsFrozen() == false {
+		if IsCallable(converterFunc) == false {
+			panic(fmt.Sprintf("%v is not callable", converterFunc))
+		}
+		r.converters[param] = converterFunc
+	}
+	return r
+}
+
+/**********************************/
+/*            CONTEXT             */
+/**********************************/
+
+// Context represents a request context in an expresso application
+type Context struct {
+	Params map[string]interface{}
+}
+
+/**********************************/
+/*               APP              */
+/**********************************/
+
+// App creates an expresso application
+func App() *Expresso {
+	return &Expresso{}
+}
+
+type registry map[string]interface{}
+
+type Expresso struct {
+	debug  bool
+	Routes []*Route
+}
+
+func (e *Expresso) SetDebug(debug bool) {
+	e.debug = debug
+}
+
+func (e Expresso) Debug() bool {
+	return e.debug
+}
+
+func (e *Expresso) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	var match *Route
+
+	for _, route := range e.Routes {
+		if route.pattern.MatchString(request.URL.Path) && route.MethodMatch(request.Method) {
+			match = route
+			break
+		}
+	}
+	if match == nil {
+		responseWriter.WriteHeader(http.StatusNotFound)
+		return
+	}
+	context := &Context{Params: map[string]interface{}{}}
+	for i, matchedParam := range match.pattern.FindStringSubmatch(request.URL.Path)[1:] {
+		context.Params[match.params[i]] = matchedParam
+	}
+
+	handlerValue := reflect.ValueOf(match.HandlerFunc())
+	arguments := make([]reflect.Value, handlerValue.Type().NumIn())
+
+	for i := 0; i < handlerValue.Type().NumIn(); i++ {
+
+		switch handlerValue.Type().In(i) {
+		case reflect.TypeOf(request):
+			arguments[i] = reflect.ValueOf(request)
+		case reflect.TypeOf(context):
+			arguments[i] = reflect.ValueOf(context)
+		default:
+			// try to inject a ResponseWriter
+			if handlerValue.Type().In(i).Implements(reflect.TypeOf((*http.ResponseWriter)(nil)).Elem()) {
+				arguments[i] = reflect.ValueOf(responseWriter)
+			} else {
+				panic(fmt.Sprintf("cannot find argument of type %+v to inject", handlerValue.Type().In(i).String()))
+			}
+		}
+	}
+
+	handlerValue.Call(arguments)
+}
+
+func (e *Expresso) Get(path string, handlerFunc interface{}) *Route {
+	route := e.Match(path, handlerFunc)
+	route.SetMethods([]string{"GET", "HEAD"})
+	return route
+}
+
+func (e *Expresso) Post(path string, handlerFunc interface{}) *Route {
+	route := e.Match(path, handlerFunc)
+	route.SetMethods([]string{"POST"})
+	return route
+}
+
+func (e *Expresso) Put(path string, handlerFunc interface{}) *Route {
+	route := e.Match(path, handlerFunc)
+	route.SetMethods([]string{"PUT"})
+	return route
+}
+
+func (e *Expresso) Delete(path string, handlerFunc interface{}) *Route {
+	route := e.Match(path, handlerFunc)
+	route.SetMethods([]string{"DELETE"})
+	return route
+}
+func (e *Expresso) Match(path string, handlerFunc interface{}) *Route {
+	route := &Route{
+		methods:    []string{"*"},
+		params:     []string{},
+		converters: map[string]interface{}{},
+	}
+	route.SetHandlerFunc(handlerFunc)
+	// extract route variables
+	routeVarsRegexp := regexp.MustCompile(Pattern)
+	matches := routeVarsRegexp.FindAllStringSubmatch(path, -1)
+	if matches != nil && len(matches) > 0 {
+		for _, match := range matches {
+			for _, subMatch := range match[1:] {
+				route.params = append(route.params, subMatch)
+			}
+		}
+	}
+	route.pattern = regexp.MustCompile(routeVarsRegexp.ReplaceAllString(path, DefaultParamPattern))
+	e.Routes = append(e.Routes, route)
+	return route
+}
+
+/**********************************/
+/*               UTIL             */
+/**********************************/
+
+func IsCallable(value interface{}) bool {
+	return reflect.ValueOf(value).Kind() == reflect.Func
 }
