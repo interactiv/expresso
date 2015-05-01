c234f78f6330258486ab982c413528a2edd59448
mparaiso <aikah@free.fr>
2015-05-01 14:27:58 +0200

    doc corrected,example added

--- doc.go
+++ doc.go
@@ -1,13 +1,14 @@
 // Copyrights 2015 mparaiso <mparaiso@online.fr>
 // License MIT
 
-// Expresso 
-// 
+// Expresso
+//
 // version 0.1
 //
 // The most awesome nano webframework for Go
 //
 // Quick start:
+//
 //
 //    package main
 //
@@ -19,7 +20,25 @@
 //    	"github.com/interactiv/expresso"
 //    )
 //
+//    //somewhere in the file the following types are declared :
+//
+//    type User struct {
+//    	Name string
+//    	Id   string
+//    }
+//
+//    type Users struct{}
+//
+//    func (_ Users) GetById(id string) *User {
+//    	users := map[string]*User{
+//    		"100": &User{Name: "John", Id: "100"},
+//    		"200": &User{Name: "Jane", Id: "200"},
+//    	}
+//    	return users[id]
+//    }
+//
 //    func main() {
+//    	var users Users
 //    	/* creates a new expresso app*/
 //    	app := expresso.New()
 //
@@ -28,15 +47,15 @@
 //    		t0 := time.Now()
 //    		next()
 //    		t1 := time.Now()
-//    		fmt.Print("lapse: ", t1.Sub(t0))
+//    		fmt.Println("lapse: ", t1.Sub(t0))
 //    	})
 //    	/*
 //    	   creates a route with a request variable called name
 //    	   handler's arguments are automatically injected, so the framework
 //    	   is fully compatible with the default http.HandlerFunc type.
 //    	*/
-//    	app.Get("/:name", func(ctx *expresso.Context, rw http.ResponseWriter) {
-//    		rw.Write([]byte("Hello" + ctx.RequestVars["name"]))
+//    	app.Get("/greet/:name?", func(ctx *expresso.Context, rw http.ResponseWriter) {
+//    		rw.Write([]byte("Hello " + ctx.RequestVars["name"].(string)))
 //    	}).
 //    		// Assert the name variable is made of alpha characters
 //    		Assert("name", "[A-Z a-z]+")
@@ -45,7 +64,7 @@
 //    	*/
 //    	adminRoutes := expresso.NewRouteCollection()
 //
-//    	adminRoutes.Use("/", func(rw http.ResponseWriter, r *http.Request) {
+//    	adminRoutes.Use("/", func(rw http.ResponseWriter, r *http.Request, next expresso.Next) {
 //    		if r.URL.Query().Get("password") != "secret" {
 //    			http.Redirect(rw, r, "/", http.StatusForbidden)
 //    			return
@@ -53,42 +72,30 @@
 //    		next()
 //    	})
 //
+//    	/*
+//    	   We use dependency injection to make our app aware of the users value
+//    	   Everytime we will inject the Users type, it will resolve the users
+//    	   value
+//    	*/
+//    	app.Injector().Register(users)
 //
-//somewhere in the file the following types are declared :
+//    	// The Convert method converts a request variable with the help
+//    	// of a converter function,allowing to use a custom type
+//    	// directly in a request handler,arguments are injected
+//    	// with the help of the Injector, the user request
+//    	// variable is passed as a string
+//    	adminRoutes.All("/:user", func(ctx *expresso.Context) {
+//    		// sends a JSON response to the client
+//    		ctx.WriteJSON(ctx.RequestVars["user"].(*User))
+//    	}).Convert("user", func(user string, users Users) *User {
+//    		return users.GetById(user)
+//    	}).Assert("user", "\\d+")
 //
-//    type User struct{
-//        Name string
-//        Id string
-//    }
-//    type Users struct{}
-//    func (_ Users)GetById(string id)*User{ ... }
+//    	//register subroute to the main route with prefix /admin
+//    	app.Mount("/admin", adminRoutes)
 //
-//    var users Users
-//
-//    We use dependency injection to make our app aware of the users value
-//    Everytime we will inject the Users type, it will resolve the users
-//    value
-//
-//    app.Injector().Register(users)
-//
-// The Convert method converts a request variable  with the help of a
-// converter function,allowing to use a custom type directly in a request
-// handler,arguments are injected with the help of the Injector, the user
-// request variable is passed as a string
-//
-//    adminRoutes.All("/:user", func(ctx *expresso.Context) {
-//        ctx.WriteString("Hello admin"+ctx.RequestVars["user"].(User).Name)
-//    }).Convert("user",func(user string,users Users)User{
-//        return users.GetById(user)
-//    })
-//
-// register subroute to the main route with prefix /admin
-//
-//    app.Mount("/admin", subRoute)
-//
-// start the webserver on port 80
-//
-//    http.ListenAndServe(":80", app)
+//    	//start the webserver on port 80
+//    	http.ListenAndServe(":80", app)
 //
 //    }
 //
---
+++ examples/quickstart/quickstart.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/interactiv/expresso"
+)
+
+//somewhere in the file the following types are declared :
+
+type User struct {
+	Name string
+	Id   string
+}
+
+type Users struct{}
+
+func (_ Users) GetById(id string) *User {
+	users := map[string]*User{
+		"100": &User{Name: "John", Id: "100"},
+		"200": &User{Name: "Jane", Id: "200"},
+	}
+	return users[id]
+}
+
+func main() {
+	var users Users
+	/* creates a new expresso app*/
+	app := expresso.New()
+
+	/* creates a middleware that will be called with each request*/
+	app.Use("/", func(next expresso.Next) {
+		t0 := time.Now()
+		next()
+		t1 := time.Now()
+		fmt.Println("lapse: ", t1.Sub(t0))
+	})
+	/*
+	   creates a route with a request variable called name
+	   handler's arguments are automatically injected, so the framework
+	   is fully compatible with the default http.HandlerFunc type.
+	*/
+	app.Get("/greet/:name?", func(ctx *expresso.Context, rw http.ResponseWriter) {
+		rw.Write([]byte("Hello " + ctx.RequestVars["name"].(string)))
+	}).
+		// Assert the name variable is made of alpha characters
+		Assert("name", "[A-Z a-z]+")
+	/*
+	   create a new route collection
+	*/
+	adminRoutes := expresso.NewRouteCollection()
+
+	adminRoutes.Use("/", func(rw http.ResponseWriter, r *http.Request, next expresso.Next) {
+		if r.URL.Query().Get("password") != "secret" {
+			http.Redirect(rw, r, "/", http.StatusForbidden)
+			return
+		}
+		next()
+	})
+
+	/*
+	   We use dependency injection to make our app aware of the users value
+	   Everytime we will inject the Users type, it will resolve the users
+	   value
+	*/
+	app.Injector().Register(users)
+
+	// The Convert method converts a request variable with the help
+	// of a converter function,allowing to use a custom type
+	// directly in a request handler,arguments are injected
+	// with the help of the Injector, the user request
+	// variable is passed as a string
+	adminRoutes.All("/:user", func(ctx *expresso.Context) {
+		// sends a JSON response to the client
+		ctx.WriteJSON(ctx.RequestVars["user"].(*User))
+	}).Convert("user", func(user string, users Users) *User {
+		return users.GetById(user)
+	}).Assert("user", "\\d+")
+
+	//register subroute to the main route with prefix /admin
+	app.Mount("/admin", adminRoutes)
+
+	//start the webserver on port 80
+	http.ListenAndServe(":80", app)
+
+}
